9ce7d69d461842a16956e052f2626f19619ea268
reaganodhiambo <reaganodhiambo003@gmail.com>
2022-05-21 12:17:32 +0300

    js basics

---
+++ src/index.js
@@ -0,0 +1,157 @@
+let x = document.getElementById("myID");
+let y = document.getElementsByClassName("myClass");
+
+x.innerHTML = "Hello...";
+// x.innerText = "<p>Hello</p>";
+y[0].innerHTML = "<p>Hello</p>";
+
+//Sum
+var sums = 15 + 20;
+console.log("Summation is " + sums);
+
+//mutiply
+var multiplication = 15 * 4;
+console.log("multiplication: " + multiplication);
+
+//Division
+var divide = 20 / 5;
+console.log("Division : " + divide);
+
+//modulus
+var modulus = 7 % 3;
+console.log("Modulus : " + modulus)
+
+//String Interpolation
+
+const name = "Fadhelah";
+console.error(`Welcome ${name})`)
+
+
+// // //Functions
+// // function sample() {
+// //     console.error("Error")
+// // }
+
+// // function sample2() {
+// //     console.error("Error");
+// //     return {
+// //         "name" : "Fadhelah"
+// //     }
+// // }
+
+// // function sample3(name) {
+// //     console.error("I said my name is " + name)
+// // }
+
+// // const sample4 = () => {
+// //     console.debug("Hello sample 4");
+// // }
+
+// // // Conditions
+
+
+// // let sample1, sample9 = 34
+// // if (sample1 > sample9) {
+// //     console.log("Greater than")
+// // }
+// // if (sample1 < sample2) {
+// //     console.log("Less than ")
+// // }
+
+
+// // let marks = 12;
+// // if (marks > 40 && marks < 60) {
+// //     console.log("less than")
+// // }
+// // let input = "y"
+// // if (names == "yes" || names == "y") {
+// //     console.log("continue")
+// // }
+
+
+const names = window.prompt("Input your name");
+if (names == null || names == undefined) {
+    window.alert("No name found")
+} else if (names.length == 0) {
+    window.alert("Name is empty")
+} else {
+    window.alert(`Your name is ${names}`)
+}
+
+
+const wantToContinue = window.prompt("Do you wish to proceed?")
+if (wantToContinue != "N") {
+    console.log("You typed yes ...")
+}
+
+const sampleInput = false;
+if (!sampleInput) {
+    console.log("Sample input is false")
+}
+
+let sampleInput2;
+if (sampleInput2) {
+    console.log("Sample has data")
+} else {
+    console.log("Sample has no data")
+}
+
+// Arrays && loops
+const element = [1, 2, 3, 4, 5];
+for (let index = 0; index < Array.length; index++) {
+    const element = Array[index]
+    console.log("At number " + element)
+}
+
+const letters = ["a", "b", "c"];
+for (const x of letters) {
+    console.log("At letter " + x)
+};
+
+const city = ["Nairobi", "Kisumu", "Mombasa"];
+city.forEach(element => {
+    console.log("At city " + element)
+})
+
+//reduce
+const xxx = [1, 2, 3, 4, 5]
+const sum2 = xxx.reduce((result, item) => {
+    return result + item;
+}, 0);
+console.log(sum2)
+
+const arr1 = ['Cecilie', 'Lone'];
+const arr2 = ['Emil', 'Tobias', 'Linus'];
+const arr3 = ['Robin', 'Morgan'];
+
+const myChildren = arr1.concat(arr2, arr3);
+console.log("Name after merge: " + myChildren);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Boolean
+Null Type
+BigInt
+Undefined type
+String */
+
+// var _hello = "Welcome";
+// let _hello2 = "Welcome Again";
+// const _hello3 = "Again, Welcome";
+// let hello5 = "";
+
+// let _hello4;
+
+// console.log(_hello4); //undefined
+// console.log("============");
+// console.log(hello5) //null
