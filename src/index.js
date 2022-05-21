9c14b339bf2cd6ae2b7d68e5ce2b9de42f700d14
reaganodhiambo <reaganodhiambo003@gmail.com>
2022-05-21 14:36:04 +0300

    Js basics

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-let x = document.getElementById("myID");
-let y = document.getElementsByClassName("myClass");
+// let x = document.getElementById("myID");
+// let y = document.getElementsByClassName("myClass");
 
-x.innerHTML = "Hello...";
-// x.innerText = "<p>Hello</p>";
-y[0].innerHTML = "<p>Hello</p>";
+// x.innerHTML = "Hello...";
+// // x.innerText = "<p>Hello</p>";
+// y[0].innerHTML = "<p>Hello</p>";
 
-//Sum
-var sums = 15 + 20;
-console.log("Summation is " + sums);
+// //Sum
+// var sums = 15 + 20;
+// console.log("Summation is " + sums);
 
-//mutiply
-var multiplication = 15 * 4;
-console.log("multiplication: " + multiplication);
+// //mutiply
+// var multiplication = 15 * 4;
+// console.log("multiplication: " + multiplication);
 
-//Division
-var divide = 20 / 5;
-console.log("Division : " + divide);
+// //Division
+// var divide = 20 / 5;
+// console.log("Division : " + divide);
 
-//modulus
-var modulus = 7 % 3;
-console.log("Modulus : " + modulus)
+// //modulus
+// var modulus = 7 % 3;
+// console.log("Modulus : " + modulus)
 
-//String Interpolation
+// //String Interpolation
 
-const name = "Fadhelah";
-console.error(`Welcome ${name})`)
+// const name = "Fadhelah";
+// console.error(`Welcome ${name})`)
 
 
 // // //Functions
@@ -69,74 +69,63 @@ console.error(`Welcome ${name})`)
 // // }
 
 
-const names = window.prompt("Input your name");
-if (names == null || names == undefined) {
-    window.alert("No name found")
-} else if (names.length == 0) {
-    window.alert("Name is empty")
-} else {
-    window.alert(`Your name is ${names}`)
-}
+// const names = window.prompt("Input your name");
+// if (names == null || names == undefined) {
+//     window.alert("No name found")
+// } else if (names.length == 0) {
+//     window.alert("Name is empty")
+// } else {
+//     window.alert(`Your name is ${names}`)
+// }
 
 
-const wantToContinue = window.prompt("Do you wish to proceed?")
-if (wantToContinue != "N") {
-    console.log("You typed yes ...")
-}
+// const wantToContinue = window.prompt("Do you wish to proceed?")
+// if (wantToContinue != "N") {
+//     console.log("You typed yes ...")
+// }
 
-const sampleInput = false;
-if (!sampleInput) {
-    console.log("Sample input is false")
-}
+// const sampleInput = false;
+// if (!sampleInput) {
+//     console.log("Sample input is false")
+// }
 
-let sampleInput2;
-if (sampleInput2) {
-    console.log("Sample has data")
-} else {
-    console.log("Sample has no data")
-}
+// let sampleInput2;
+// if (sampleInput2) {
+//     console.log("Sample has data")
+// } else {
+//     console.log("Sample has no data")
+// }
 
-// Arrays && loops
-const element = [1, 2, 3, 4, 5];
-for (let index = 0; index < Array.length; index++) {
-    const element = Array[index]
-    console.log("At number " + element)
-}
+// // Arrays && loops
+// const element = [1, 2, 3, 4, 5];
+// for (let index = 0; index < Array.length; index++) {
+//     const element = Array[index]
+//     console.log("At number " + element)
+// }
 
-const letters = ["a", "b", "c"];
-for (const x of letters) {
-    console.log("At letter " + x)
-};
+// const letters = ["a", "b", "c"];
+// for (const x of letters) {
+//     console.log("At letter " + x)
+// };
 
-const city = ["Nairobi", "Kisumu", "Mombasa"];
-city.forEach(element => {
-    console.log("At city " + element)
-})
+// const city = ["Nairobi", "Kisumu", "Mombasa"];
+// city.forEach(element => {
+//     console.log("At city " + element)
+// })
 
-//reduce
-const xxx = [1, 2, 3, 4, 5]
-const sum2 = xxx.reduce((result, item) => {
-    return result + item;
-}, 0);
-console.log(sum2)
+// //reduce
+// const xxx = [1, 2, 3, 4, 5]
+// const sum2 = xxx.reduce((result, item) => {
+//     return result + item;
+// }, 0);
+// console.log(sum2)
 
-const arr1 = ['Cecilie', 'Lone'];
-const arr2 = ['Emil', 'Tobias', 'Linus'];
-const arr3 = ['Robin', 'Morgan'];
+// const arr1 = ['Cecilie', 'Lone'];
+// const arr2 = ['Emil', 'Tobias', 'Linus'];
+// const arr3 = ['Robin', 'Morgan'];
 
-const myChildren = arr1.concat(arr2, arr3);
-console.log("Name after merge: " + myChildren);
-
-
-
-
-
-
-
-
-
-
-
+// const myChildren = arr1.concat(arr2, arr3);
+// console.log("Name after merge: " + myChildren);
 
 
 /* Boolean
@@ -155,3 +144,4 @@ String */
 // console.log(_hello4); //undefined
 // console.log("============");
 // console.log(hello5) //null
+
